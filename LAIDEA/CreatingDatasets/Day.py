f5547e88cae5b036d8836a9b0d0763630f5986f0
Arnoldo <arfersan20@gmail.com>
2025-02-28 11:16:01 -0600

    Day dataframe corrections

--- LAIDEA/CreatingDatasets/Day.py
+++ LAIDEA/CreatingDatasets/Day.py
@@ -5,8 +5,12 @@ PATH = "../../Datasets/"
 df = pd.read_csv(PATH + "CompletedDataset.csv")
 df["Time"] = pd.to_datetime(df["Time"])
 
+full_range = pd.date_range(start=df["Time"].min(), end=df["Time"].max(), freq='D')
+dfFull = pd.DataFrame(full_range, columns=["Time"])
+dfFinal = dfFull.merge(df, on="Time", how="left")
+
 aggregation_functions = {
-    **{col: lambda x: x.mode().iloc[0] if not x.mode().empty else None for col in df.columns if 'flag' in col},
+    **{col: lambda x: x.mode().iloc[0] if not x.mode().empty else None for col in dfFinal.columns if 'flag' in col},
     'WDir_Avg': lambda x: round(x).mode().iloc[0] if not x.mode().empty else None,
     'Rain_Tot': 'sum',
     'WDir_SD': 'std',
