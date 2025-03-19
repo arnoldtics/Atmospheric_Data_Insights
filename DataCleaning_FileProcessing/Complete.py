8611e5d8c0c177f1416e08120f96be1db4a7ad8e
unknown <arfersan20@gmail.com>
2025-03-19 16:22:19 -0600

    Final pipeline and database

---
+++ DataCleaning_FileProcessing/Complete.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+PATH = "../Datasets/"
+
+dfMorelia = pd.read_csv(PATH + "Morelia.csv")
+dfJuriquilla = pd.read_csv(PATH + "Juriquilla.csv")
+dfMerida = pd.read_csv(PATH + "Merida.csv")
+dfHermosillo = pd.read_csv(PATH + "Hermosillo.csv")
+
+df = pd.concat([dfMorelia, dfJuriquilla, dfMerida, dfHermosillo], axis=0)
+df["Time"] = pd.to_datetime(df["Time"])
+df.sort_values("Time", inplace=True)
+
+df.to_csv(PATH + "Complete.csv", index=False)
